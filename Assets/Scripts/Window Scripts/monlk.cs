7def85ac1cba5f5623c56a6eb54f00e10e3eb8b7
ThatAnnoyingKid <57725498+ThatAnnoyingKid@users.noreply.github.com>
2022-04-01 02:25:57 -0700

    superior ui

---
+++ Assets/Scripts/Window Scripts/monlk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class monlk : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+		{
+			SceneManager.LoadScene("Office");
+		}
+		
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			Debug.Log(Global.blindClosed);
+		}
+    }
+}
