98f66e5503dab0256c489705cb72942ea4b08f19
ThatAnnoyingKid <57725498+ThatAnnoyingKid@users.noreply.github.com>
2022-05-02 15:43:14 -0700

    i will end it all

--- Assets/Scripts/Office Scriopts/TheFog.cs
+++ Assets/Scripts/Office Scriopts/TheFog.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class TheFog : MonoBehaviour
 {
-    // Start is called before the first frame update
+	private int mexican = 7;
+	private int noEyes = 7;
+	private int mexicanChance;
+	private int sight;
+	public static bool hope = false;
+	public static bool imDoingFine = false;
+	private int immigrant;
+	private int bloodThurst;
+	
+	
     void Start()
     {
-        Global.theFogIsComing = false;
+    }
+	
+	private IEnumerator Numb()
+	{
+		Global.theFogIsComing = false;
 		Global.losingInterst = true;
+		immigrant = UnityEngine.Random.Range(0,2);
+		bloodThurst = UnityEngine.Random.Range(0,2);
 		
 		while(Global.losingInterst == true)
 		{
 			StartCoroutine(Global.Timeout());
 			StartCoroutine(Global.IFeelPainEveryNight());
+			yield return new WaitForSeconds(3);
+			mexicanChance = UnityEngine.Random.Range(1,11);
+			sight = UnityEngine.Random.Range(1, 11);
+			
+			if(mexican >= mexicanChance && !hope)
+			{
+				hope = true;
+				StartCoroutine(Global.HelpMe());
+			}
+			
+			if(noEyes >= sight && !imDoingFine)
+			{
+				imDoingFine = true;
+				StartCoroutine(Global.HelpMe());
+			}
 		}
-    }
+	}
 
-    // Update is called once per frame
     void Update()
     {
         
