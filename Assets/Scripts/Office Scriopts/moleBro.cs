6c9eb7e4298d4d277441ae335d0d0f90285f37e0
ThatAnnoyingKid <aiden.a.russo123@gmail.com>
2022-05-12 10:40:24 -0700

    may god help me clense the errors and let me introduce the cutie mole lad

---
+++ Assets/Scripts/Office Scriopts/moleBro.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moleBro : MonoBehaviour
+{
+	private int ui = 7;
+	private int showChance;
+	public static bool moleShow = false;
+	public GameObject moleMan;
+	private int moleSpawn;
+	
+    void Start()
+    {
+        
+    }
+
+	public IEnumerator moleGuy() 
+	{
+		
+	}
+
+    void Update()
+    {
+        
+    }
+}
